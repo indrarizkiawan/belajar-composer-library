e60540163033758ab19d48ec74a8e9dd981fcad6
Indra Rizkiawan <indrarizkiawan@Indras-MacBook-Pro.local>
2023-05-07 15:30:31 +0700

    Add defaul value

--- src/Customer.php
+++ src/Customer.php
@@ -7,7 +7,7 @@ class Customer {
     {
 
     }
-    public function sayHello(string $name): string
+    public function sayHello(string $name = "Guest"): string
     {
         return "Hello $name, my name is $this->name";
     }
